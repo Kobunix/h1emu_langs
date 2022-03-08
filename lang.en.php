7f1200c4511afa1090d0f49d2cf89c8bd46562e6
Kobunix <kobunix@gmail.com>
2022-03-08 17:00:06 +0100

    Update lang.en.php

--- lang.en.php
+++ lang.en.php
@@ -13,31 +13,46 @@ $lang = [
 	** ***************************************************************** */
 	'GOTO'	=> "Go to",
 	
-
 	'LANG_CHOOSE'	=> "Language",
 	'FR'		=> "French",
-	'EN'		=> "English",
+	'US'		=> "English",
 	'PT'		=> "Portuguese",
-	'SV'		=> "Swedish",
+	'SE'		=> "Swedish",
 	'PL'		=> "Polish",
+	'CN'		=> "Chinese (simplified)",
 
 	/* ******************************************************************
 		MAINMENU
 	** ***************************************************************** */
 	'MM_HOME'		=> "Home",
-	'MM_COMMUNITY'	=> "Community",
-	'MM_NEWS'		=> "News",
+	'MM_COMMUNITY'	=> "Boards",
+	'MM_CHANGELOG'	=> "Updates",
 	'MM_STATUS'		=> "Serverlist",
 	'MM_DOWNLOADS'	=> "Downloads",
+	'MM_MEDIA'		=> "Media",
+	'MM_WIKI'		=> "Wiki",
 
 	/* ******************************************************************
-		PAGE CONTENT
+		PAGE CONTENT & TITLES <title>
 	** ***************************************************************** */
 	'PAGE_HOME' 			=> "Welcome to H1EMU",
 	'PAGE_DOWNLOADS'		=> "Downloads",
-	'PAGE_COCKPIT_AUTH' 	=> "Login",
+	'PAGE_MEDIA'			=> "Media",
+	'PAGE_WIKI'				=> "Wiki",
+	'PAGE_CHANGELOG'		=> "Last Update",
+	'PAGE_BOARDS'	=> "Boards",
+	'PAGE_OURTEAM'			=> "Our Team",
+
 	'PAGE_HOME_ABOUT'		=> "About the game",
-	'PAGE_HOME_ABOUT_TEXT'	=> "Together face In. His called Two lesser given divide. From, cattle saying be was doesn't set. Creature bearing life wherein dominion in saying them moveth first have. Under set darkness over light beast face fill from in after isn't first own all fowl itself evening also, grass doesn't Sea. Created very likeness herb wherein from lesser was bring brought above. Bearing tree a grass very.",
+
+
+	'DL_OPTCAT_1'	=> "Launcher",
+		'DL_OPTCAT_TXT_1'	=> "Download the launcherand <span class='text-h1'>play</span> the game",
+	'DL_OPTCAT_2'	=> "Server",
+		'DL_OPTCAT_TXT_2'	=> "Create your <span class='text-h1'>own</span> server !",
+	'DL_OPTCAT_3'	=> "Extras",
+		'DL_OPTCAT_TXT_3'	=> "Discover some cool things",
+	
 
 	/* ******************************************************************
 		STATUS PAGE
@@ -54,34 +69,106 @@ $lang = [
 	'PAGE_COMMUNITY_SERVERS'		=> "Community Servers",
 
 	/* ******************************************************************
+		CONTENT BOX
+	** ***************************************************************** */
+	'CONTENT_MEDIA_INFORMATION'	=> "All these images are captured from the project (H1Emu). No old images are shown.",
+
+	/* ******************************************************************
+		WIKI
+	** ***************************************************************** */
+		// Nav separator
+	'MS_WIKI_SEP_MANAGE'	=> "Manage",
+	'MS_WIKI_SEP_CATS'		=> "Categories",
+
+		// Nav Buttons
+	'MS_WIKI_HOME'			=> "Wiki Index",
+	'MS_WIKI_CREATEPAGE'	=> "Create Page",
+
+
+	'TITLE_WIKI_CATEGORY' => "Category",
+
+	/* ******************************************************************
+		BOARDS
+	** ***************************************************************** */
+	'BOARD_STATISTICS'	=> "Statistics",
+		'BOARD_STAT_TOTALSUBJECTS'	=> "Total Subjects",
+		'BOARD_STAT_TOTALMEMBER'	=> "Total Members",
+		'BOARD_STAT_LASTPOST'	=> "Last Post",
+
+	/* ******************************************************************
 		COCKPIT
 	** ***************************************************************** */
-	'COCKPIT_TITLE'		=> "Login required",
+	'COCKPIT_TITLE_LEGACY'	=> "Legacy Connect",
+	'COCKPIT_TITLE_THIRD'	=> "Other methods",
+	'COCKPIT_REGISTER'	=> "Create new account",
+	'COCKPIT_FORGOT'	=> "Forgotten password ?",
+
+
 	'COCKPIT_MYACCOUNT'	=> "My account",
+	'COCKPIT_REGISTER'	=> "Create new account",
 	'COCKPIT_LOGOUT'	=> "Logout",
 
+	// TAB "VIRTUALS CATEGORIES"
+	'COCKPIT_VC_PROFILE'	=> "Profile",
+	'COCKPIT_VC_ACCOUNT'	=> "Account",
+	'COCKPIT_VC_LAUNCHER'	=> "Launcher",
+
 	// MENU
-	'COCKPIT_HOME'	=> "Home",
-	'COCKPIT_FORUMPOSTS'	=> "Forum Activity",
-	'COCKPIT_SECURITY'	=> "Security",
+	'COCKPIT_HOME'			=> "Home",
+
+	'COCKPIT_ACCOUNT_EDIT'	=> "Edit Account",
+	'COCKPIT_PROFILE_PRIVACY'	=> "Privacy Settings",
+	
+	'COCKPIT_REQUESTLIST'	=> "Serverlist Request",
+
+	'COCKPIT_SECURITYLAUNCH'=> "Account Key",
+	'COCKPIT_SECURITYPASS'	=> "Change Password",
+	'COCKPIT_SECURITY2FA'	=> "2FA",
+	'COCKPIT_2FA'			=> "2FA Verification",
+
+	'COCKPIT_LOGIN'	=> "Connect",
+	// 'COCKPIT_LOGIN_STEAM'	=> "Connect with Steam",
+
+	
 
 	// TAB ACCOUNT TITLES PAGES
-	'COCKPIT_TITLE_PAGE_HOME'		=> "Account Details",
-	'COCKPIT_TITLE_PAGE_FORUMSPOST'	=> "Forum Activity",
-	'COCKPIT_TITLE_PAGE_SECURITY'	=> "Security",
+	'COCKPIT_TITLE_PAGE_HOME'		=> "Dashboard",
 
-	'COCKPIT_TITLE_SECURITY_PASSWORD'	=> "Change my Password",
+	'COCKPIT_TITLE_PAGE_ACCOUNT_EDIT'	=> "Edit my account",
 
-	// Menu Sub(DL)
-	'DL_M_CHOOSE'		=> "Please choose version",
-	'DL_M_APP'		=> "Application",
-	'DL_M_MANUAL'	=> "Separated Binaries",
+
+	'COCKPIT_TITLE_PAGE_SECURITYPASS'	=> "Change my Password",
+	'COCKPIT_TITLE_PAGE_SECURITY2FA'	=> "2FA Security",
+	'COCKPIT_TITLE_PAGE_SECURITYLAUNCH'	=> "Account Key",
+		'COCKPIT_TITLE_PAGE_LAUNCH_YOURKEYIS'		=> "Your Key is",
+		'COCKPIT_TITLE_PAGE_LAUNCH_TEXT'	=> "This key is used to login into the launcher.<br>Follow <a class='wikilink' href=''>this link</a> if you need help",
+
+		'COCKPIT_TITLE_PAGE_2FA_BACKUPCODES'		=> "Backup Codes",
+		'COCKPIT_TITLE_PAGE_2FA_BACKUPCODES_TEXT'	=> "These codes are used in emergency only, if you can't validate your 2FA Method.",
+		'COCKPIT_TITLE_PAGE_2FA_BACKUPCODES_BTN'	=> "Generate my Emergency Codes",
+
+	
+
+
 	/* ******************************************************************
-		MAINMENU
+		====== COMMON ======
 	** ***************************************************************** */
 
+
+	/* ======================
+		@TABLES
+	** ====================== */
+	'TABLE_NAME'		=> "Name",
+	'TABLE_REVIEWBY'	=> "Reviewed by",
+	'TABLE_SUPPORTEDV'	=> "Supported Version",
+	'TABLE_INFOS'		=> "Infos",
+
+
+	/* ======================
+		@ERROR
+	** ====================== */
 	'ERROR_PAGENOTFOUND'		=> "This page no longer exists",
-	'ERROR_PAGEACCESSDENIED'	=> "This page is disabled. Please try again later...",
+	'ERROR_PAGEACCESSDENIED'	=> "You do not have permission to view this page.",
 
 	'ERROR_REQUIRED_LOGIN'	=> "Login is required to perform actions here.",
 	'ERROR_INDEV'	=> "This website is under development, please be patient.",
@@ -89,31 +176,118 @@ $lang = [
 	'ERROR_PAGENOTFULL'	=> "This page is not finished, links are not fully working !",
 	'ERROR_NOSERVERS'	=> "No server here. Do you want to host a server ?",
 
+		'ERROR_EMPTYFORMS'	=> "This form can't be empty.",
+		'ERROR_IDENTICALVALUES'	=> "Data seems to be identical",
+
+
+	'ERROR_LOGIN_INVALIDCREDENTIALS'	=> "Invalid credentials, did you <a href='".Url('cockpit/forgot/')."'>forgot your password ?</a>",
+	
+	'ERROR_ACCOUNT_ACTIVATED'			=> "Your account is now active.",
+	
+	'ERROR_REGISTER_PASSWORDNOTMATCH'		=> "Password mismatch",
+	'ERROR_REGISTER_INVALIDFORMAT'			=> "Invalid Format",
+	'ERROR_REGISTER_USERNAMEALREADYEXIST'	=> "This username is already used",
+	'ERROR_REGISTER_EMAILALREADYEXIST'		=> "This email is already used. Try <a href='".Url('cockpit/login/')."'>connecting</a>",
+	'ERROR_REGISTER_SUCCESS'				=> "Your account is now created. Check your emails.",
+
 	'ERROR_INFOSETUP'	=> "A full documentation can be found <a class='text-danger' href='//wiki.h1emu.com/'>here</a>.",
-	'ERROR_USELASTRELEASE' => "Actual Project version -> ".$conf['EMU_RELEASE_VERSION']."",
+	'ERROR_USELASTRELEASE' => "Actual Project version ->",
+	
+	'ERROR_ACCOUNT_INFO_LEGACY'	=> "Account Type - <strong>LEGACY</strong>",
+	'ERROR_ACCOUNT_INFO_STEAM'	=> "Account Type - <strong>STEAM</strong>",
+
+	'ERROR_REGISTER_PASSWORDNOTGOOD'	=> "Password mismatch",
+
+	'ERROR_RESETPASSWORD_MAILSENT'	=> "We've sent you an email with a code",
+	'ERROR_RESETPASSWORD_INVALIDNEWPASS'	=> "Passwords mismatch",
+	'ERROR_RESETPASSWORD_INVALIDCODE'	=> "The entered code is invalid",
+
+	'ERROR_TABLE_EMPTY'	=> "No data here",
+
+
 	/* ******************************************************************
 		INPUTS PLACEHOLDERS
 	** ***************************************************************** */
 	'INPUT_USERNAME'	=> "Username",
 	'INPUT_PASSWORD'	=> "Password",
+	'INPUT_PASSWORD_CONF'	=> "Password (Confirmation)",
+	'INPUT_EMAIL'		=> "Email",
+
+	'INPUT_ACTUALPASS'	=> "Actual Password",
+	'INPUT_NEWPASS'		=> "New Password",
+	'INPUT_NEWPASSCONF'	=> "New Password (confirmation)",
+	'INPUT_SERVERNAME'	=> "ServerName",
+	'INPUT_FINALIZE'	=> "Finalize Request",
 
 	'SLIDER_HOMETEXT'	=> "H1Z1 EMULATOR",
 
+	'INPUT_WIKIPAGENAME'	=> "Wiki page name",
+
+	'INPUT_NEWSCONTENT'		=> "News content...",
+	'INPUT_RESPONSECONTENT'	=> "Write your answer...",
+	'INPUT_WIKICONTENT'		=> "Wiki page content...",
+
+	'INPUT_NEWPICTURE'	=> "Upload",
+
+	/* ******************************************************************
+		CONSOLE
+	** ***************************************************************** */
+
+	// Sidenav Menu
+	'CONSOLE_HOME'		=> "Dashboard",
+	'CONSOLE_NEWS'		=> "News",
+	'CONSOLE_SERVERS'	=> "Servers",
+	'CONSOLE_USERS'		=> "Users",
+	'CONSOLE_CONFIG'	=> "Settings",
+
+	// Counters
+	'COUNTER_TOTAL_NEWS'	=> "Total News",
+	'COUNTER_TOTAL_SERVERS'	=> "Total Servers",
+	'COUNTER_TOTAL_USERS'	=> "Total Users",
+	'COUNTER_LASTUPDT'	=> "Last release",
+
+	// Titles pages
+	'CONSOLE_TITLE_PAGE_HOME'	=> "Dashboard Admin",
+	'CONSOLE_TITLE_PAGE_NEWS'	=> "Manage News",
+	'CONSOLE_TITLE_PAGE_SERVERS'=> "Manage Servers",
+	'CONSOLE_TITLE_PAGE_USERS'	=> "Manage Users",
+	'CONSOLE_TITLE_PAGE_CONFIG'	=> "Website Settings",
+
+	'CONSOLE_TITLE_PAGE_EDITUSER'	=> "Editing user",
+	'CONSOLE_TITLE_PAGE_EDITNEWS'	=> "Editing News",
+	'CONSOLE_NEWS_ADD'	=> "Add news",
+
+	'SAVE'		=> "Save",
+	'ENABLED'	=> "Enabled",
+	'DISABLED'	=> "Disabled",
 	/* ******************************************************************
 		DATE SYSTEM
 	** ***************************************************************** */
 	'DATE_SECOND'	=> "Second",
 	'DATE_MINUTES'	=> "Minute",
 	'DATE_HOUR'		=> "Hour",
-	'DATE_DAYS'		=> "Jour",
+	'DATE_DAYS'		=> "Day",
 	'DATE_MONTH'	=> "Month",
 	'DATE_YEAR'		=> "Year",
 
 
+	// Words
+	'EMULATOR' => "Emulator",
+	'FAMILY' => "Family",
+	'COMMUNITY' => "Community",
+
+	'YES'	=> "Yes",
+	'NO'	=> "No",
+	'SUBMIT'	=> "Submit",
+	'COPY'	=> "Copy",
 	'BACK2'	=> "Back",
 	'BACK'	=> "Back to home",
-
-	'HOST_MSGE'	=> "Need a performance host ? Use this service <a href=''>".($conf['hostperf']) ? $conf['hostperf']:"NoHostDefined"."</a>",
+	'ERROR_GCODE'	=> "Game Error",
+	'UUID_RESET'	=> "Reset UUID",
+	'UUID_COPYED'	=> "Copied !",
+	'CREATED_AT'	=> "Created at",
+	'IN'	=> "in",
+	// 'HOST_MSGE'	=> "Need a performance host ? Use this service <a href=''>".($conf['hostperf']) ? $conf['hostperf']:"NoHostDefined"."</a>",
 
 ];
 return $lang;
